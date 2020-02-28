b5150f093d996e81fed6556a7a0aa8e64a00959c
Silvio Wanderson Florentino <silvio.wflorentino@sp.senac.br>
2020-02-27 22:36:28 -0300

    novas classes e procedures + banco

---
+++ ConcessionariaSenac2020/Control/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcessionariaSenac2020.Control
+{
+    class Conexao
+    {
+        public static string conectar()
+        {
+            return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\silvio.wflorentino\Desktop\Silvio_Florentino\C#_Noite\Solution_ConcessionariaSenac2020\ConcessionariaSenac2020\bdconcessionariasenac.mdf;Integrated Security=True ";
+        }
+    }
+}
---
+++ ConcessionariaSenac2020/Control/ManipulaVeiculos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using ConcessionariaSenac2020.Model;
+using System.Windows.Forms;
+
+namespace ConcessionariaSenac2020.Control
+{
+    class ManipulaVeiculos
+    {
+
+        public void cadastrarVeiculos()
+        {
+            SqlConnection cn = new SqlConnection(Conexao.conectar());
+            SqlCommand cmd = new SqlCommand("pCadastrarVeiculos",cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+           
+
+            try
+            {
+                cmd.Parameters.AddWithValue("@marca",Veiculos.Marca);
+                cmd.Parameters.AddWithValue("@modelo", Veiculos.Modelo);
+                cmd.Parameters.AddWithValue("@cor",Veiculos.Cor);
+                cmd.Parameters.AddWithValue("@placa",Veiculos.Placa);
+
+                SqlParameter Agatha = cmd.Parameters.Add("@codigo", SqlDbType.Int);
+                Agatha.Direction = ParameterDirection.Output;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro realizado com Sucesso");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+     
+
+    }
+
+   
+
+    
+}
